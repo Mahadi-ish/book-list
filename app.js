3b6e4a64e0f6e992eb3673201c47e80f1656a5e8
Mahadi Hassan Shohag <mahadi@ishraak.com>
2021-07-04 01:32:34 +0600

    title change

--- app.js
+++ app.js
@@ -1,12 +1,8 @@
 document.getElementById('add-book').addEventListener('click', function(e){
     e.preventDefault();
-    //console.log('clicked hoyci');
     const title = document.getElementById('title').value;
     const author = document.getElementById('author').value;
-    //console.log(author);
     const year = document.getElementById('year').value;
-    //console.log(title);
-
     if(title == '' && author == '' && year == ''){
         alert('Please input something');
     }
@@ -15,14 +11,12 @@ document.getElementById('add-book').addEventListener('click', function(e){
         const newTitle = document.createElement('th');
         newTitle.innerText = title;
         newRow.append(newTitle);
-        //console.log(newTitle);
         const newAuthor = document.createElement('th');
         newAuthor.innerText = author;
         newRow.append(newAuthor);
         const newYear = document.createElement('th');
         newYear.innerText = year;
         newRow.append(newYear);
-
         const newBookList = document.getElementById('book-list');
         newBookList.append(newRow);
 
